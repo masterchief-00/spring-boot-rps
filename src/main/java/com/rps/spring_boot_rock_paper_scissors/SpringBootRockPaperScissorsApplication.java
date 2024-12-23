465de0170b9330adfab2077be24864554ca15264
Kwizera Pacifique <kwizerapacifique19@gmail.com>
2024-12-23 14:45:38 +0200

    init commit

---
+++ src/main/java/com/rps/spring_boot_rock_paper_scissors/SpringBootRockPaperScissorsApplication.java
@@ -0,0 +1,13 @@
+package com.rps.spring_boot_rock_paper_scissors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRockPaperScissorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRockPaperScissorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rps/spring_boot_rock_paper_scissors/SpringBootRockPaperScissorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rps.spring_boot_rock_paper_scissors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRockPaperScissorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
